35479e4e9d4fd87d550bba49781cbb64062ac453
Henrique Eidt <henriqueidt@gmail.com>
2019-05-27 12:37:08 -0300

    fix sw file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw_cached_full_app.js')
+      .register('../sw_cached_page.js')
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log(error))
   })
