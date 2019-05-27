3f923fb1bf5bcf43ed660b9c058751392b80f593
Henrique Eidt <henriqueidt@gmail.com>
2019-05-27 12:34:22 -0300

    change to full page cache

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw_cached_page.js')
+      .register('../sw_cached_full_app.js')
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log(error))
   })
